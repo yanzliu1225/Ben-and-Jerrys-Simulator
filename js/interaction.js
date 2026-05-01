8fdf904b0fbcdbefcfaa2d54aad0359c852c6374
Yan Zhen Liu <yliu1225@umd.edu>
2026-05-01 13:47:56 -0400

    Update interaction.js (speed)

--- js/interaction.js
+++ js/interaction.js
@@ -64,7 +64,7 @@ export function checkInteraction(camera) {
 }
 
 export function updateMovement() {
-  const speed = 0.8;
+  const speed = 1;
 
   if (controls && controls.isLocked) {
     if (keys.w) controls.moveForward(speed);
